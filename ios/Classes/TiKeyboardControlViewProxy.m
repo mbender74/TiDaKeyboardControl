6cfc9701b0f5016ee2016e659d0f7f400c52adda
mbender74 <marc_bender@icloud.com>
2026-06-08 17:22:39 +0200

    Adjust damping to 0.6

--- ios/Classes/TiKeyboardControlViewProxy.m
+++ ios/Classes/TiKeyboardControlViewProxy.m
@@ -1512,7 +1512,7 @@ static inline UIViewAnimationOptions AnimationOptionsForCurve(UIViewAnimationCur
             __strong TiKeyboardControlViewProxy *strongSelf = self;
             [UIView animateWithDuration:toolbarDuration
                                     delay:delay
-                        usingSpringWithDamping:0.85
+                        usingSpringWithDamping:0.6
                               initialSpringVelocity:0.0
                                             options:UIViewAnimationOptionBeginFromCurrentState
                                          animations:^{
@@ -1521,7 +1521,7 @@ static inline UIViewAnimationOptions AnimationOptionsForCurve(UIViewAnimationCur
             } completion:^(BOOL finished) {
                 NSLog(@"[TOOLBAR] Anim complete: finalTransform.ty=%.1f", strongSelf->toolbarview.layer.affineTransform.ty);
             }];
-            NSLog(@"[TOOLBAR] Spring anim: toolbarDist=%.1f keyboardDist=%.1f delay=%.3fs duration=%.3fs damping=0.85", 
+            NSLog(@"[TOOLBAR] Spring anim: toolbarDist=%.1f keyboardDist=%.1f delay=%.3fs duration=%.3fs damping=0.6", 
                   toolbarDistance, keyboardDistance, delay, toolbarDuration);
         }
         self->settledShift = trans;
